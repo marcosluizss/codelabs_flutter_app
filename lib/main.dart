d9dd9712c8a1d49d5771c1a5927bd19eaefa2625
Marcos Luiz <marcosluizss@gmail.com>
2021-02-03 17:49:48 -0300

    Adiciona navegação para segunda tela

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,13 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome to Flutter"),
+        title: const Text("Startup Name Generator"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -72,6 +78,36 @@ class _RandomWordsState extends State<RandomWords> {
           }
         });
       },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      return buildSavedPage();
+    }));
+  }
+
+  Widget buildSavedPage() {
+    final tiles = _saved.map((WordPair pair) {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+    });
+    final divided = ListTile.divideTiles(
+      context: context,
+      tiles: tiles,
+    ).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Saved Suggestions"),
+      ),
+      body: ListView(
+        children: divided,
+      ),
     );
   }
 }
